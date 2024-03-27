6e3827e98ba001b6665460e6995a56f8ca7cac55
Devessier <baptiste@devessier.fr>
2024-03-27 11:25:47 +0100

    feat: add background image

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,7 +1,4 @@
-import {AbsoluteFill} from 'remotion';
-import {Logo} from './Logo';
-import {Subtitle} from './Subtitle';
-import {Title} from './Title';
+import {AbsoluteFill, Img, staticFile} from 'remotion';
 import {z} from 'zod';
 import {zColor} from '@remotion/zod-types';
 
@@ -11,18 +8,13 @@ export const myCompSchema = z.object({
 	logoColor: zColor(),
 });
 
-export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
-	titleText: propOne,
-	titleColor: propTwo,
-	logoColor: propThree,
-}) => {
+export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({}) => {
 	return (
-		<AbsoluteFill className="bg-gray-100 items-center justify-center">
-			<div className="m-10" />
-			<Logo logoColor={propThree} />
-			<div className="m-3" />
-			<Title titleText={propOne} titleColor={propTwo} />
-			<Subtitle />
+		<AbsoluteFill>
+			<Img
+				src={staticFile('/background.jpg')}
+				className="h-full w-full object-cover object-center"
+			/>
 		</AbsoluteFill>
 	);
 };
