f330beb19f20e1d15a5016330c1f50d368c67f38
Devessier <baptiste@devessier.fr>
2024-04-10 01:22:37 +0200

    feat: animate the rings

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,16 +1,20 @@
-import {AbsoluteFill, Audio, Img, staticFile, useCurrentFrame} from 'remotion';
+import {useVideoConfig} from 'remotion';
+import {useCurrentFrame} from 'remotion';
+import {AbsoluteFill, Audio, Img, Sequence, spring, staticFile} from 'remotion';
 import {z} from 'zod';
 
 export const myCompSchema = z.object({
-	frames: z.array(z.array(z.string())),
+	speakers: z.record(
+		z.array(z.object({startFrame: z.number(), stopFrame: z.number()}))
+	),
 });
 
-export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
-	frames,
-}) => {
-	const frame = useCurrentFrame();
+const EXIT_ANIMATION_DURATION_IN_FRAMES = 15;
 
-	const speakers = [
+export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
+	speakers,
+}) => {
+	const speakersMetadata = [
 		{
 			name: 'Adam',
 			color: '#0b0b0b',
@@ -43,26 +47,43 @@ export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
 			<AbsoluteFill>
 				<div className="bg-zinc-900 mx-12 my-10 h-full rounded-md shadow-2xl">
 					<div className="grid grid-cols-3 grid-rows-1 p-4 gap-x-2 h-full">
-						{speakers.map((speaker, index) => (
+						{speakersMetadata.map((speakerMetadata, index) => (
 							<div
 								key={index}
-								style={{backgroundColor: speaker.color}}
+								style={{backgroundColor: speakerMetadata.color}}
 								className="grid grid-rows-[1fr,auto] rounded-md"
 							>
 								<div className="flex justify-center items-center">
-									<Img
-										src={speaker.picture}
-										className={`size-32 rounded-full shadow-xl ${
-											frames[frame].includes(speaker.speakerLabel) === true
-												? 'ring-4 ring-green-400'
-												: ''
-										}`}
-									/>
+									<div className="relative">
+										<Img
+											src={speakerMetadata.picture}
+											className="size-32 rounded-full shadow-xl"
+										/>
+
+										{speakers[speakerMetadata.speakerLabel].map(
+											(segment, index) => (
+												<Sequence
+													key={index}
+													from={segment.startFrame}
+													durationInFrames={
+														segment.stopFrame -
+														segment.startFrame +
+														EXIT_ANIMATION_DURATION_IN_FRAMES // Be sure the exit animation won't be truncated by extending the duration of the sequence
+													}
+												>
+													<GreenRing
+														startFrame={segment.startFrame}
+														stopFrame={segment.stopFrame}
+													/>
+												</Sequence>
+											)
+										)}
+									</div>
 								</div>
 
 								<div>
 									<p className="px-2 py-0.5 text-white bg-zinc-900/20">
-										{speaker.name}
+										{speakerMetadata.name}
 									</p>
 								</div>
 							</div>
@@ -75,3 +96,45 @@ export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
 		</>
 	);
 };
+
+function GreenRing({
+	startFrame,
+	stopFrame,
+}: {
+	startFrame: number;
+	stopFrame: number;
+}) {
+	const frame = useCurrentFrame();
+	const {fps} = useVideoConfig();
+
+	const enter = spring({
+		frame,
+		fps,
+		durationInFrames: EXIT_ANIMATION_DURATION_IN_FRAMES,
+	});
+	const exit = spring({
+		frame,
+		fps,
+		durationInFrames: EXIT_ANIMATION_DURATION_IN_FRAMES,
+		/**
+		 * The delay will make the exit animation start after the segment ends.
+		 * This will give the look that the animation starts when the speakers
+		 * starts talking, but takes some time to fade out when the speaker stops
+		 * talking.
+		 */
+		delay: stopFrame - startFrame,
+	});
+
+	/**
+	 * See this post about making an exit animation with Remotion:
+	 * https://discord.com/channels/809501355504959528/1205135451812536431/1205135451812536431
+	 */
+	const animation = enter - exit;
+
+	return (
+		<div
+			className="w-full h-full rounded-full ring-green-400 ring-4"
+			style={{opacity: animation}}
+		/>
+	);
+}
